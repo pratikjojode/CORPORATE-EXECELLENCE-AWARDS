4d708bae40e683f7ad672ee20353ac18d30b3d80
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-06-08 12:21:35 +0530

    okay

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -9,7 +9,7 @@ import Sponsors from "../Components/Sponsors";
 import Footer from "../Components/Footer";
 
 import ModiPage from "../Components/ModiPage";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../Components/HeroSection";
 
 const HomePage = () => {
   const [showLocationPopup, setShowLocationPopup] = useState(false);
